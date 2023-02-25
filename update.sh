d54b3a5f7ba6bc63a0dcd31c9f43ca3867ee8031
henderjon <list@box370.net>
2023-02-25 00:25:07 -0600

    assets to resources; drop digrc; new vsc setting

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 DOTPATH=$(pwd)
-echo "rm \"$DOTPATH/assets/vcs-settings.json\""
-echo "cp \"$HOME/Library/Application Support/Code/User/settings.json\" \"$DOTPATH/assets/vcs-settings.json\""
+echo "rm \"$DOTPATH/resources/vcs-settings.json\""
+echo "cp \"$HOME/Library/Application Support/Code/User/settings.json\" \"$DOTPATH/resources/vcs-settings.json\""
 
-echo "rm \"$DOTPATH/assets/Brewfile\""
-echo "brew bundle dump --file=\"$DOTPATH/assets/Brewfile\""
+echo "rm \"$DOTPATH/resources/Brewfile\""
+echo "brew bundle dump --file=\"$DOTPATH/resources/Brewfile\""

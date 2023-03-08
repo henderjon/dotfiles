f89b6c1415f9e6fcbc48ca51583042d81dc22753
henderjon <list@box370.net>
2023-03-08 15:40:01 -0600

    organizes resources/

--- update.sh
+++ update.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env sh
 
 DOTPATH=$(pwd)
-echo "rm \"$DOTPATH/resources/vcs-settings.json\""
-echo "cp \"$HOME/Library/Application Support/Code/User/settings.json\" \"$DOTPATH/resources/vcs-settings.json\""
+VCS_SETTINGS_FILE="vcs-settings.json"
+echo "rm \"$DOTPATH/resources/vscode/$VCS_SETTINGS_FILE\""
+echo "cp \"$HOME/Library/Application Support/Code/User/settings.json\" \"$DOTPATH/resources/vscode/$VCS_SETTINGS_FILE\""
 
-echo "rm \"$DOTPATH/resources/Brewfile\""
-echo "brew bundle dump --file=\"$DOTPATH/resources/Brewfile\""
+SUBL_COLOR_SETTINGS="Monokai22.sublime-color-scheme"
+echo "rm \"$DOTPATH/resources/sublime_text/$SUBL_COLOR_SETTINGS\""
+echo "cp \"$HOME/Library/Application Support/Sublime Text/Packages/User/$SUBL_COLOR_SETTINGS\" \"$DOTPATH/resources/sublime_text/$SUBL_COLOR_SETTINGS\""
+
+SUBL_SETTINGS_FILE="Preferences.sublime-settings"
+echo "rm \"$DOTPATH/resources/sublime_text/$SUBL_SETTINGS_FILE\""
+echo "cp \"$HOME/Library/Application Support/Sublime Text/Packages/User/$SUBL_SETTINGS_FILE\" \"$DOTPATH/resources/sublime_text/$SUBL_SETTINGS_FILE\""
+
+echo "rm \"$DOTPATH/resources/brew/Brewfile\""
+echo "brew bundle dump --file=\"$DOTPATH/resources/brew/Brewfile\""
+
+

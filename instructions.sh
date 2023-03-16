27364107a5fe9c937615ef1c0ced3b32cbe62338
henderjon <list@box370.net>
2023-03-16 08:56:44 -0500

    adds stderr/stdout to instructions to allow for piping; rearrange vscode settings

--- instructions.sh
+++ instructions.sh
@@ -5,53 +5,63 @@
 set -e
 
 notice () {
-	printf "\033[01;36m%s\033[00;00m\n" "$1"
+	NOTICE=$(printf "\033[01;36m%s\033[00;00m\n" "$1")
+	ste "$NOTICE"
+}
+
+ste () {
+	echo "$1" 1>&2;
+}
+
+sto () {
+	echo "$1"
 }
 
 _mv_file_rm_sym () {
 	if [ -f "$1" ] || [ -d "$1" ]; then
 		if [ ! -h "$1" ]; then
-			echo "mv \"$1\" \"$1.bkup.$(date +%s)\""
+			sto "mv \"$1\" \"$1.bkup.$(date +%s)\""
 		else
-			echo "rm \"$1\""
+			sto "rm \"$1\""
 		fi
 	fi
 }
 
 _no_folder_create() {
 	if [ ! -d "$1" ]; then
-		echo "mkdir \"$1\""
+		sto "mkdir \"$1\""
 	fi
 }
 
 _cfg_ln(){
 	_mv_file_rm_sym "$2"
-	echo "ln -s \"$1\" \"$2\""
+	sto "ln -s \"$1\" \"$2\""
 }
 
-echo "please name me:"
+ste "please name me:"
 read -r ENV_NAME
 if [ -z "$ENV_NAME" ]; then
-	echo "you forgot a name ... "
+	sto "you forgot a name ... "
 	exit 1
 else
-	echo "Hello, $ENV_NAME"
+	ste "Hello, $ENV_NAME"
 fi
 
 DOTPATH=$(pwd)
 # ------
 notice "# ------ zsh"
-echo "chsh -s /bin/zsh"
+sto "chsh -s /bin/zsh"
 # ------
 notice "# ------ gitconfig"
+sto "git config --global --replace-all include.path \"$DOTPATH/conf/git/gitconfig\""
+
 if [ -z "$CODESPACES" ]; then #codespaces doesn't like these GPG values
-	echo "git config --global --replace-all include.path \"$DOTPATH/conf/git/gitconfig\" \"$DOTPATH/conf/git/gitgpg\""
-else
-	echo "git config --global --replace-all include.path \"$DOTPATH/conf/git/gitconfig\""
+	sto "git config --global --add include.path \"$DOTPATH/conf/git/gitgpg\""
 fi
+
 notice "# !----- edit signing key and email in gitconfig & gitgpg"
-echo "git config --global user.email \"...\""
-echo "git config --global signingkey \"...\""
+sto "git config --global user.email \"...\""
+sto "git config --global signingkey \"...\""
 # ------
 notice "# ------ rc files"
 for RCFILE in "$DOTPATH"/rc/*
@@ -61,16 +71,16 @@ do
 done
 # ------
 notice "# ------ zshenv"
-echo "touch \"$HOME/.zshenv\""
-echo "echo 'export _LOCAL_ENV_NAME=$ENV_NAME' >> \"$HOME/.zshenv\""
+sto "touch \"$HOME/.zshenv\""
+sto "echo 'export _LOCAL_ENV_NAME=$ENV_NAME' >> \"$HOME/.zshenv\""
 # ------
 notice "# ------ bin files"
 _no_folder_create "$HOME/bin"
 # ------
 notice "# ------ ssh"
 _no_folder_create "$HOME/.ssh"
-echo "ssh-keygen -t ed25519 -C \"henderjon; $ENV_NAME; $(date '+%FT%T%z')\""
-echo "curl -L \"https://github.com/henderjon.keys\" >> \"$HOME/.ssh/authorized_keys\""
+sto "ssh-keygen -t ed25519 -C \"henderjon; $ENV_NAME; $(date '+%FT%T%z')\""
+sto "curl -L \"https://github.com/henderjon.keys\" >> \"$HOME/.ssh/authorized_keys\""
 # ------
 if [[ "$OSTYPE" == darwin* ]]; then
 	notice "# ------ macos dictionary"
@@ -78,21 +88,21 @@ if [[ "$OSTYPE" == darwin* ]]; then
 
 	if [ ! -x "$(which brew)" ]; then
 		notice "# ------ brew"
-		echo "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+		sto "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
 	fi
 	notice "# ------ brew bundle"
-	echo "brew bundle --file=$DOTPATH/assets/Brewfile"
+	sto "brew bundle --file=$DOTPATH/assets/Brewfile"
 	notice "# ------ vscode"
-	echo "code --install-extension \"$DOTPATH/assets/monokai22-1.0.0.vsix\""
+	sto "code --install-extension \"$DOTPATH/assets/monokai22-1.0.0.vsix\""
 	_mv_file_rm_sym "$HOME/Library/Application Support/Code/User/settings.json"
-	echo "cp \"$DOTPATH/assets/vcs-settings.json\" \"$HOME/Library/Application Support/Code/User/settings.json\""
+	sto "cp \"$DOTPATH/assets/vcs-settings.json\" \"$HOME/Library/Application Support/Code/User/settings.json\""
 fi
 # ------
 if [ -x "$(which php)" ]; then
 	if [ ! -x "$(which composer)" ]; then
 		notice "# ------ composer"
-		echo "curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=\"$HOME/bin/\""
-		echo "mv \"$HOME/bin/composer.phar\" \"$HOME/bin/composer\""
+		sto "curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=\"$HOME/bin/\""
+		sto "mv \"$HOME/bin/composer.phar\" \"$HOME/bin/composer\""
 	fi
 else
 	notice "# ------ php"
@@ -100,5 +110,5 @@ else
 fi
 
 notice "# ------ cleanup; avoids clobbering on fetch/pull"
-echo "git checkout -b \"$ENV_NAME\""
+sto "git checkout -b \"$ENV_NAME\""
 

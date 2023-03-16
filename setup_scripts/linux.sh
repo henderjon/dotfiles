4048d2e2983c761b3decbe84beb85652de106108
henderjon <list@box370.net>
2023-03-16 17:29:39 -0500

    notices should be cyan

--- setup_scripts/linux.sh
+++ setup_scripts/linux.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ "$OSTYPE" == linux* ]]; then
 	notice "# ------ vscode"
-	ste "assuming this is cli, don't do anything with VSCode"
+	notice "assuming this is cli, don't do anything with VSCode"
 	# _mv_file_rm_sym "$HOME/.config/Code/User/settings.json"
 	# sto "cp \"$DOTPATH/assets/vcs-settings.json\" \"$HOME/.config/Code/User/settings.json\""
 fi

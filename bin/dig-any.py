f0f0ff7c129985ef14724b9a023006ff2d9e41a2
henderjon <list@box370.net>
2023-03-17 12:51:27 -0500

    adds input() to dig-any.py

--- bin/dig-any.py
+++ bin/dig-any.py
@@ -5,15 +5,19 @@ import os, sys
 types = [
     "NS", "A", "AAAA", "CNAME", "MX", "TXT",
 ]
+if len(sys.argv) <= 1 :
+    domain = input("domain to query: ")
+else:
+    domain = sys.argv[1]
 
-domain = sys.argv[1]
 single_type = sys.argv[2] if len(sys.argv) > 2 else ""
+
 for type in types:
     if single_type != "" and single_type != type:
         continue
 
     print(f"# ------ {type}")
-    _ = os.system(f"dig +nocmd {domain} +noall +answer {type}")
+    os.system(f"dig +nocmd {domain} +noall +answer {type}")
     # os.system(f"dig {domain} +nocomments +noall +answer -t {type}")
 
 # https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site

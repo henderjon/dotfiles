8e53490d773d75cdce3294b59536678cb654eb80
henderjon <list@box370.net>
2023-03-17 09:23:07 -0500

    adds script to use dig effectively

---
+++ bin/dig-any.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+types = [
+    "NS", "A", "AAAA", "CNAME", "MX", "TXT",
+]
+
+domain = sys.argv[1]
+single_type = sys.argv[2] if len(sys.argv) > 2 else ""
+for type in types:
+    if single_type != "" and single_type != type:
+        continue
+
+    print(f"# ------ {type}")
+    _ = os.system(f"dig +nocmd {domain} +noall +answer {type}")
+    # os.system(f"dig {domain} +nocomments +noall +answer -t {type}")
+
+# https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site
+# dig EXAMPLE.COM +noall +answer -t AAAA
+# dig WWW.EXAMPLE.COM +nostats +nocomments +nocmd

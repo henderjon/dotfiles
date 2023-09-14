c2ca22dd228068c24fb606eedef5353d3113a2ea
henderjon <list@box370.net>
2023-09-14 16:08:42 -0500

    tweaks git config and go-release

--- bin/go-release.py
+++ bin/go-release.py
@@ -4,10 +4,15 @@ import sys
 import subprocess
 
 if len(sys.argv) <= 1 or sys.argv[1] == "":
-	print("please provide a tag")
+	print("please provide a tag; include the 'v', if you please")
 	exit(1)
 
 tag=sys.argv[1]
+
+if tag[0] != "v":
+	print("please provide a tag; include the 'v', if you please")
+	exit(1)
+
 # tags = subprocess.check_output(["git", "tag", "--sort", "-version:refname", '--format="%(refname:strip=2);%(symref)"'])
 # info=tags[:tags.index(b"\n")]
 # tag=info[:info.index(b";")]
@@ -24,7 +29,7 @@ builds=[
 for build in builds:
 	print(f'GOOS={build["os"]} GOARCH={build["arch"]} make release')
 
-hash = tags = subprocess.check_output(["git", "rev-list", "-n", "1", tag])
+hash = subprocess.check_output(["git", "rev-list", "-n", "1", tag])
 hash = hash[:7]
 print(f"gh release create {tag}")
 print(f"gh release upload {tag} ./release/*-{str(hash, 'utf-8')}-*.tgz")
